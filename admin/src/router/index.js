dcceaae3ab6b5694304a8f8c41bd8257a71773d7
zxn <2889062858@qq.com>
2024-07-10 20:21:06 +0800

    login框架

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,8 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL), // 动态生成url
   routes: [
+    {
+      path:'/login',
+      name:'登录',
+      component:() => import('../views/login.vue') // 懒加载
+    }
   ]
 })
 
---
+++ admin/src/views/login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="login-body">
+    <div class="left-bg"></div>
+    <div class="right-panel">
+      <el-form class="login-form">
+        <div class="register">
+          <div class="title">后台管理系统登录</div>
+          <el-form-item prop="phone"></el-form-item>
+          <el-form-item prop="password"></el-form-item>
+          <el-form-item prop="checkCode"></el-form-item>
+          <el-form-item prop="rememberMe"></el-form-item>
+          <el-form-item prop="btn"></el-form-item>
+        </div>
+      </el-form>
+    </div>
+  </div>
+</template>
+    
+<script setup>
+</script>
+    
+<style>
+</style>
