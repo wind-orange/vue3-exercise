c79c7ce201264f218fb500105e85fed19a13148d
zxn <2889062858@qq.com>
2024-07-16 14:48:22 +0800

    全局引入vue-cookies

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,13 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 import './assets/base.scss'
 import "@/assets/icon/iconfont.css"
 
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 import Request from "@/utils/Request"
 import Message from "@/utils/Message"
+import VueCookies from "vue-cookies"
 
 const app = createApp(App)
 
@@ -16,5 +19,6 @@ app.use(ElementPlus)
 
 app.config.globalProperties.Request=Request
 app.config.globalProperties.Message=Message
+app.config.globalProperties.VueCookies=VueCookies
 
 app.mount('#app')
