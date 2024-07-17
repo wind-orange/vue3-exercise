20ebbe9ab1d8ec9bc260c53f9b11e9beb37a6c20
zxn <2889062858@qq.com>
2024-07-17 16:12:51 +0800

    Layout页面数据渲染

--- admin/src/views/Lauout.vue
+++ admin/src/views/Lauout.vue
@@ -2,22 +2,61 @@
   <div class="layout">
     <div class="header">
       <div class="logo">Exercise管理后台</div>
-      <div class="userInfo">欢迎回来</div>
+      <div class="userinfo">
+        欢迎回来，
+        <el-dropdown>
+          <span class="name">{{ userInfo.userName }}</span>
+          <template #dropdown>
+            <el-dropdown-menu>
+              <el-dropdown-item @click="updateMyPwd">修改密码</el-dropdown-item>
+              <el-dropdown-item @click="logout">退出</el-dropdown-item>
+            </el-dropdown-menu>
+          </template>
+        </el-dropdown>
+      </div>
     </div>
     <div class="body">
       <div class="left-sider">
-        <div class="menu-list"></div>
-        <div class="menu-sub-list"></div>
+        <div class="menu-list">
+          <div
+            :class="['menu-item',currentPmenu.menuUrl == item.menuUrl ? 'active' : '']"
+            v-for="item in userInfo.menuList"
+            @click="pMenuClickHander(item)"
+          >
+            <div :class="['iconfont', 'icon-' + item.icon]"></div>
+            <div class="menu-name">{{ item.menuName }}</div>
+          </div>
+        </div>
+        <div class="menu-sub-list">
+          <div
+            :class="['sub-menu',currentSubMenu.menuUrl == sub.menuUrl ? 'active' : '']"
+            v-for="sub in currentPmenu.children"
+            @click="jump(sub)"
+          >
+            {{ sub.menuName }}
+          </div>
+        </div>
       </div>
       <div class="main-content">
         <div class="tab-content"></div>
-        <div class="body-content"></div>
+        <div class="body-content">
+          <Router-view></Router-view>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { getCurrentInstance, nextTick, onMounted, ref } from "vue";
+const userInfo = ref(JSON.parse(sessionStorage.getItem("userInfo")) || { menuList: [] })
+const currentPmenu = ref({})
+const currentSubMenu = ref({})
+
+const updateMyPwd = () => {}
+const logout = () => {}
+const jump = () => {}
+</script>
 
 <style>
 .layout {
@@ -40,6 +79,11 @@
       font-size: 14px;
       display: flex;
       align-items: center;
+      .name {
+        font-size: 1;
+        color: #049eff;
+        cursor: pointer;
+      }
     }
   }
   .body {
@@ -54,11 +98,44 @@
         width: 70px;
         text-align: center;
         background: #1a1a1a;
+        .menu-item {
+          padding: 15px 0;
+          cursor: pointer;
+          color: #fff;
+          .iconfont {
+            font-size: 20px;
+          }
+          .menu-name {
+            font-weight: bold;
+          }
+          &:hover {
+            color: #06a7ff;
+          }
+        }
+        .active {
+          background: #06a7ff;
+          &:hover {
+            color: #fff;
+          }
+        }
       }
       .menu-sub-list {
         flex: 1;
         position: relative;
         padding: 5px 5px;
+        .sub-menu {
+          margin-top: 5px;
+          cursor: pointer;
+          padding: 10px 8px;
+          border-radius: 5px;
+          &:hover {
+            color: #05a1f5;
+          }
+        }
+        .active {
+          background: #e8f4ff;
+          color: #1890ff;
+        }
       }
     }
     .main-content {
