5a498aef84205028c400037f291dc453239385d6
zxn <2889062858@qq.com>
2024-08-05 11:04:11 +0800

    渲染Table数据

--- admin/src/views/settings/Role.vue
+++ admin/src/views/settings/Role.vue
@@ -1,7 +1,8 @@
 <template>
+  <!-- 上方搜索框 -->
   <div class="top-panel">
     <el-card>
-      <el-form label-width="70px" label-position="right">
+      <el-form :model="searchForm" label-width="70px" label-position="right">
         <!-- 搜索框 -->
         <el-row>
           <el-col :span="5">
@@ -10,6 +11,7 @@
                 class="password-input"
                 clearable
                 placeholder="支持模糊搜索"
+                @keyup.enter.native="loadDataList"
               ></el-input>
             </el-form-item>
           </el-col>
@@ -19,6 +21,7 @@
                 class="password-input"
                 clearable
                 placeholder="支持模糊搜索"
+                @keyup.enter.native="loadDataList"
               ></el-input>
             </el-form-item>
           </el-col>
@@ -29,38 +32,117 @@
         </el-row>
       </el-form>
     </el-card>
-    <!-- 下方content -->
-    <el-row
-      :gutter="20"
-      :style="{ 'margin-top': '10px' }"
-      class="tabel-data-card"
-    >
-      <!-- 左侧角色列表 -->
-      <el-col :span="18">
-        <el-card class="box-card">
-          <template #header>
-            <span>角色列表</span>
-          </template>
-        </el-card>
-      </el-col>
-      <!-- 右侧菜单栏 -->
-      <el-col :span="6">
-        <el-card class="box-card">
-          <template #header>
-            <span>菜单信息</span>
-            <el-button
-              type="primary"
-              :style="{ float: 'right' }"
-              >保存
-            </el-button>
-          </template>
-        </el-card>
-      </el-col>
-    </el-row>
   </div>
+  <!-- 下方content -->
+  <el-row
+    :gutter="20"
+    :style="{ 'margin-top': '10px' }"
+    class="tabel-data-card"
+  >
+    <!-- 左侧角色列表 -->
+    <el-col :span="18">
+      <el-card class="box-card">
+        <template #header>
+          <span>角色列表</span>
+        </template>
+        <Table
+          ref="tableInfoRef"
+          :columns="columns"
+          :fetch="loadDataList"
+          :dataSource="tableData"
+          :options="tableOptions"
+          :extHeight="tableOptions.extHeight"
+          @row-click="handleRowClick"
+        >
+          <template #operation="{ index, row }">
+            <div class="row-op-panel">
+              <a class="a-link" href="javascript:void(0)">修改 </a>
+              <a class="a-link" href="javascript:void(0)">删除 </a>
+            </div>
+          </template>
+        </Table>
+      </el-card>
+    </el-col>
+    <!-- 右侧菜单栏 -->
+    <el-col :span="6">
+      <el-card class="box-card">
+        <template #header>
+          <span>菜单信息</span>
+          <el-button type="primary" :style="{ float: 'right' }"
+            >保存
+          </el-button>
+        </template>
+      </el-card>
+    </el-col>
+  </el-row>
 </template>
 
 <script setup>
+import Table from "@/components/Table.vue";
+import { getCurrentInstance, ref } from "vue";
+
+const { proxy } = getCurrentInstance();
+const api = {
+  loadRole: "/settings/loadRoles",
+  loadMenu: "/settings/menuList",
+  delRole: "/settings/delRole",
+  saveRoleMenu: "/settings/saveRoleMenu",
+  roleDetail: "/settings/getRoleByRoleId",
+};
+const tableInfoRef = ref();
+const tableOptions = ref({
+  extHeight: 125,
+});
+// Table字段
+const columns = [
+  {
+    label: "角色名称",
+    prop: "roleName",
+  },
+  {
+    label: "描述",
+    prop: "roleDesc",
+  },
+  {
+    label: "创建时间",
+    prop: "createTime",
+  },
+  {
+    width: 100,
+    label: "操作",
+    scopedSlots: "operation",
+  },
+];
+
+const searchForm = ref();
+const tableData = ref({});
+const currentRow = ref({}); // 当前选中的行
+// 获取数据
+const loadDataList = async () => {
+  let parmas = {
+    pageNo: tableData.value.pageNo,
+    pageSize: tableData.value.pageSize,
+  };
+  // 搜索框
+  Object.assign(parmas, searchForm.value);
+  let result = await proxy.Request({
+    url: api.loadRole,
+    parmas: parmas,
+  });
+  if (!result) return;
+  Object.assign(tableData.value, result.data);
+  // 默认选中第一个
+  if (
+    Object.keys(currentRow.value).length === 0 &&
+    result.data.list.length > 0
+  ) {
+    Object.assign(currentRow.value, result.data.list[0]);
+    handleRowClick(currentRow.value)
+  }
+  tableInfoRef.value.setCurrentRow("roleId", currentRow.value.roleId);
+};
+// 选中列表渲染相应菜单
+const handleRowClick=(row)=>{}
 </script>
 
 <style lang="scss" scoped>
