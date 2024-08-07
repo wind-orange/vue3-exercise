c1ec23b1fcbde635d7104a5feef472466ba1b6f3
zxn <2889062858@qq.com>
2024-08-07 15:47:03 +0800

    has指令

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,32 +1,46 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-import './assets/base.scss'
-import "@/assets/icon/iconfont.css"
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import "./assets/base.scss";
+import "@/assets/icon/iconfont.css";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
 
-import Request from "@/utils/Request"
-import Message from "@/utils/Message"
-import Confirm from "@/utils/Confirm"
-import Verify from "@/utils/Verify"
+import Request from "@/utils/Request";
+import Message from "@/utils/Message";
+import Confirm from "@/utils/Confirm";
+import Verify from "@/utils/Verify";
+import PermissionCode from "@/utils/PermissionCode";
 
-import VueCookies from "vue-cookies"
+import VueCookies from "vue-cookies";
 
-import Dialog from './components/Dialog.vue'
-import Table from './components/Table.vue'
-const app = createApp(App)
+import Dialog from "./components/Dialog.vue";
+import Table from "./components/Table.vue";
+const app = createApp(App);
 
-app.use(router)
-app.use(ElementPlus)
-app.component("Dialog",Dialog)
-app.component("Table",Table)
+app.use(router);
+app.use(ElementPlus);
+app.component("Dialog", Dialog);
+app.component("Table", Table);
 
-app.config.globalProperties.Request=Request
-app.config.globalProperties.Message=Message
-app.config.globalProperties.Confirm=Confirm
-app.config.globalProperties.Verify=Verify
-app.config.globalProperties.VueCookies=VueCookies
+app.config.globalProperties.Request = Request;
+app.config.globalProperties.Message = Message;
+app.config.globalProperties.Confirm = Confirm;
+app.config.globalProperties.Verify = Verify;
+app.config.globalProperties.VueCookies = VueCookies;
+app.config.globalProperties.PermissionCode = PermissionCode;
 
-app.mount('#app')
+app.directive("has", {
+  mounted: (el, binding, vnode) => {
+    let userInfo = JSON.parse(sessionStorage.getItem("userInfo"));
+    let PermissionCodeList = userInfo.PermissionCodeList;
+    PermissionCodeList =
+      PermissionCodeList == undefined ? [] : PermissionCodeList;
+    if (!PermissionCodeList.includes(binding.value)) {
+      el.parentNode.removeChild(el);
+    }
+  },
+});
+
+app.mount("#app");
